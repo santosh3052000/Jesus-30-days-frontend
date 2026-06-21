be717e37a078c14b5f5571a55f09e37cc226e1f1
santosh3052000 <santosh3052000@gmail.com>
2026-06-21 19:07:31 +0530

    base fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000', // Forwards /api requests to your backend
